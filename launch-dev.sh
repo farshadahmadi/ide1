9db42d7490d2c5c8052d7ec802afa4c2f96a2aaf
Antti Nieminen <anttihn@gmail.com>
2016-03-14 14:57:59 +0200

    Build for 1acfe4839971e2fee1a2fed5255c8872cbefdde6

--- launch-dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-PORT=8081 NODE_ENV=development npm start 2> logi-err.log > logi.log &
--- launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-PORT=8080 NODE_ENV=production npm start 2> logi-err.log > logi.log &
