ac482079ac1a10b25bed9cf7e0f363d19288ca06
farshadahmadi <farshad.ahmadi.gh@gmail.com>
2016-06-02 15:18:47 +0300

    Build for 08c8af8a1f2aee5c03954d1fbc24b94eb084ef68

--- examplepackage/main.js
+++ examplepackage/main.js
@@ -6,36 +6,31 @@ function MainClass(){
   Agent.call(this);
 }
 
+var greeting = "";
+
 MainClass.prototype = Object.create(Agent.prototype);
 MainClass.prototype.constructor = MainClass;
 
-MainClass.prototype.preStartFunction = function(startMain){
-    console.log("pre-start");
+MainClass.prototype.initialize = function(startMain){
+    greeting = "World!";
     startMain();
 }
 
-MainClass.prototype.preStopFunction = function(stopExecution){
-    console.log("pre-stop");
-    stopExecution("");
+MainClass.prototype.main = function(restartMain) {
+    console.log("hello " + greeting);
+    restartMain();
 }
 
-MainClass.prototype.mainFunction = function(restartMain) {
-    console.log("helloOoOo!");
-    restartMain();
+MainClass.prototype.terminate = function(stopExecution){
+    console.log("See you " + greeting);
+    stopExecution("");
 }
 
 function createAgentObject() {
   var obj = new MainClass();
-  obj.setWork(obj.mainFunction, true, 1000);
-  obj.setPreStopWork(obj.preStopFunction);
-  obj.setPreStartWork(obj.preStartFunction);
+  obj.configureInterval(true, 1000);
   return obj;
 }
 
 // Do not change basepath
 var basePath = "/api";
-
-// receives HTTP GET request sent to http://<deviceurl>:8000/app/<app-id>/api/sayhello>
-app.get(basePath + "/sayhello", function(req, res){
-  res.send("hello!!!");
-});
