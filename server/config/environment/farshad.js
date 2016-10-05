ad90185fc32f399a5e84a04ef76c8ade0636caa7
farshadahmadi <farshad.ahmadi.gh@gmail.com>
2016-10-05 15:27:54 +0300

    Build for d9638431276dcc5cefe84186e125dbf3a67ce0be

--- server/config/environment/farshad.js
+++ server/config/environment/farshad.js
@@ -1,16 +1,16 @@
-'use strict';
+'use strict'
 
 // Development specific configuration
 // ==================================
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/koodain-dev'
-  },
-
+          uri: 'mongodb://localhost/koodain-dev'
+    },
+  
   git: {
-    projects: '/home/liquid-iot/Desktop/workspace'
-  },
-
+          projects: '/home/liquid-iot/Desktop/workspace'
+    },
+  
   seedDB: false
 };
