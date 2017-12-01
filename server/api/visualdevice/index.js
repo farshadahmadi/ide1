bba16c6d6f97a06e44ea569888eab986df5ad20a
farshadahmadi <farshad.ahmadi.gh@gmail.com>
2017-12-01 13:14:49 +0200

    60e4016 now when a host is deleted or a host is redeployed, its information is also deleted from resource registry

--- server/api/visualdevice/index.js
+++ server/api/visualdevice/index.js
@@ -8,5 +8,7 @@ var router = express.Router();
 router.get('/', hostCtrl.list);
 router.post('/', hostCtrl.create);
 router.delete('/:host', hostCtrl.remove);
+router.post('/:host', hostCtrl.trigger);
+router.put('/:host/deployment', hostCtrl.triggerDeployment);
 
 module.exports = router;
